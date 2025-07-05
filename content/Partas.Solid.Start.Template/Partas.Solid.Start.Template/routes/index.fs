9f3ed434a37e7495248fb8a55060fc2b4cc60cb7
cabboose <shayanftw@gmail.com>
2025-07-06 01:40:13 +0800

    Initial commit

---
+++ content/Partas.Solid.Start.Template/Partas.Solid.Start.Template/routes/index.fs
@@ -0,0 +1,13 @@
+﻿module Partas.Solid.Start.Template.routes.index
+
+open Partas.Solid
+open Partas.Solid.Meta
+open Fable.Core
+
+[<ExportDefault>]
+[<SolidComponent>]
+let private Home() =
+    main() {
+        Title() { "Partas.Solid.Start.Template" }
+        h1() { "Partas.Solid.Start.Template" }
+    }
---
+++ content/Partas.Solid.Start.Template/app.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "@solidjs/start/config";
+import devtools from "solid-devtools/vite";
+
+export default defineConfig({
+    // Change to project name as required
+    appRoot: "./Partas.Solid.Start.Template",
+    routeDir: "./routes",
+    // Recommended to use .fs.jsx extension so that fable clean will not
+    // wipe out solid-start files
+    extensions: [ 'js', 'jsx' , 'ts', 'tsx', 'fs.jsx' ],
+    // Vite config
+    vite: {
+        plugins:[
+            devtools({
+                autoname: true
+            })
+        ],
+        server: {
+            watch: {
+                ignore: [
+                    "**/*.md",
+                    "**/*.fs",
+                    "**/*.fsx"
+                ]
+            }
+        }
+    },
+    // Vinxi/Nitro/SolidStart
+    server: {
+        // preset: "vercel"
+    }
+});
